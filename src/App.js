3c3a6d7bba092f70a6edce10845941455af32f39
BPitts8019 <bradleyj.pitts@gmail.com>
2019-09-10 21:55:31 -0700

    Added static JSX for Member Cards and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-// import {Button, Form, FormGroup, Label, Input, FormText, FormFeedback} from "reactstrap"
+import {Card, CardHeader, CardBody, CardTitle, CardText} from "reactstrap"
 import './App.css';
 
 import CreateMember from './components/CreateMember.js';
@@ -20,8 +20,20 @@ function App() {
          <h1>Team Builder</h1>
          <CreateMember addMember={addTeamMember} />
 
-         <div className="dispay">
-            {team.map((member, index) => <div key={index}>{member.firstName}</div>)}
+         <div className="display">
+            {/* {team.map((member, index) => <div key={index}>{member.firstName}</div>)} */}
+            <div className="member-card">
+               <h4 className="mc-title">Hello World</h4>
+               <div className="mc-body">This is the body</div>
+            </div>
+            <div className="member-card">
+               <h4 className="mc-title">Hello World</h4>
+               <div className="mc-body">This is the body</div>
+            </div>
+            <div className="member-card">
+               <h4 className="mc-title">Hello World</h4>
+               <div className="mc-body">This is the body</div>
+            </div>
          </div>
       </div>
    );
